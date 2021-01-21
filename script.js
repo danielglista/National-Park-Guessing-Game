301601c9b4145f6ffa279d35bda1104cc8f20c6d
danielglista <dnglista+github@gmail.com>
2021-01-20 23:04:37 -0500

    added scoring system and added state names to answer screen

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const wipeTransitionTime = 1000;
+const wipeTransitionTime = 1000; // in milliseconds
 
 function getNPSData(callback) {
     fetch('https://developer.nps.gov/api/v1/parks?limit=500', {
@@ -52,6 +52,9 @@ function renderQuestion(park) {
 
 
 function displayQuestion() {
+    document.querySelector('.btn-next').classList.add('disabled');
+    document.querySelector('.question-counter').innerHTML = parseInt(document.querySelector('.question-counter').innerHTML) + 1;
+
     document.querySelector('.answer-mask').style.width = 0;
     document.querySelector('.text-input').value = '';
 
@@ -62,37 +65,43 @@ function displayQuestion() {
         document.querySelector('.answer-mask').style.width = '100%';
         document.querySelector('.btn-submit').classList.remove('disabled');
     }, wipeTransitionTime)
+
+    document.querySelector('.score').innerHTML = document.querySelector('.score').getAttribute('data-score');
 }
 
 
-function renderAnswer(correctAnswer) {
+function renderAnswer(correctAnswer, park) {
     const userAnswer = getStateTwoDigitCode(document.querySelector('.text-input').value);
     if (correctAnswer.includes(userAnswer)) {
         console.log('yay!')
         document.querySelector('.answer-container').className = document.querySelector('.answer-container').className.replace('red', 'green');
         document.querySelector('.answer-container i').className = 'far fa-check-square fa-9x';
-        document.querySelector('.answer-container h1').className = '';
-        document.querySelector('.answer-container h1').innerHTML = '+100';
+        document.querySelector('.answer-container h1').innerHTML = '+1';
         document.querySelector('.answer-container input').classList.add('btn-green-outline');
         document.querySelector('.answer-container input').classList.remove('btn-red-outline');
-        // document.querySelector('.answer-container').style.left = '0';
+        document.querySelector('.score').setAttribute('data-score', parseInt(document.querySelector('.score').innerHTML) + 1)
     } else {
         document.querySelector('.answer-container').className = document.querySelector('.answer-container').className.replace('green', 'red');
         document.querySelector('.answer-container i').className = 'fas fa-times fa-9x';
-        document.querySelector('.answer-container h1').className = '';
         document.querySelector('.answer-container h1').innerHTML = '';
         document.querySelector('.answer-container input').classList.add('btn-red-outline');
         document.querySelector('.answer-container input').classList.remove('btn-green-outline');
-        // document.querySelector('.answer-container').style.left = '0';
     }
 
-    return true;
+    let correctStates = correctAnswer.split(',');
+    let stateNamesString = '';
+    for (let i in correctStates) {
+        if (i > 0) {
+            stateNamesString += ', ';
+        }
+        stateNamesString += getStateFullName(correctStates[i]);
+    }
+    document.querySelector('.state-answer').innerHTML = stateNamesString + ' - ' + park.fullName.replace(/national|preserve|park/ig, '');
 }
 
 function displayAnsewr() {
     document.querySelector('.question-mask').style.width = 0;
     document.querySelector('.btn-submit').classList.add('disabled');
-    document.querySelector('.btn-next').classList.add('disabled');
 
     setTimeout( () => {
         document.querySelector('.question-mask').style.zIndex = 5;
@@ -115,27 +124,31 @@ getNPSData( (jsonRes) => {
 
     for (let i in data) {
         if (data[i].designation.includes("National Park")) parks.push(data[i]);  
+        // Manually adding American Samoa, Redwood, and New River because the NPS API is inconsistant with its categorization
+        if (data[i].fullName == 'National Park of American Samoa' || data[i].fullName == 'Redwood National and State Parks' || data[i].fullName == 'New River Gorge National River') parks.push(data[i]);
     }
 
     let parkIterator = 0;
     const numberOfParks = parks.length;
+    document.querySelector('.question-total').innerHTML = numberOfParks;
+
     generateRandomParkOrder(parks);
      
-    let correctAnswer = renderQuestion(parks[parkIterator++]);
+    let correctAnswer = renderQuestion(parks[parkIterator]);
 
 
     document.addEventListener('keypress', (e) => {
         if (e.key == 'Enter') {
-            renderAnswer(correctAnswer);
+            renderAnswer(correctAnswer, parks[parkIterator++]);
             displayAnsewr();
-            setTimeout(() => {correctAnswer = renderQuestion(parks[parkIterator++])}, wipeTransitionTime );
+            setTimeout(() => {correctAnswer = renderQuestion(parks[parkIterator])}, wipeTransitionTime );
         }
     })
 
     document.querySelector('.btn-submit').addEventListener('click', (e) => {
-        renderAnswer(correctAnswer);
+        renderAnswer(correctAnswer, parks[parkIterator++]);
         displayAnsewr();
-        setTimeout(() => {correctAnswer = renderQuestion(parks[parkIterator++])}, wipeTransitionTime );
+        setTimeout(() => {correctAnswer = renderQuestion(parks[parkIterator])}, wipeTransitionTime );
     });
 
     document.querySelector('.btn-next').addEventListener('click', () => {
