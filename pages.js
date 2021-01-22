5e16cd132f149608cfb97e1af2eb1e408fd356cd
danielglista <dnglista+github@gmail.com>
2021-01-22 16:10:27 -0500

    added a menu

---
+++ pages.js
@@ -0,0 +1,141 @@
+function renderMenuPage() {
+    return `
+    <div class="menu-container">
+        <h1 class='green bold text-center'>National Park Guessing Game</h1>
+        <p class='green'>Test your geographic knowledge of our national parks by matching national parks with the state they reside in. You will get the park name, picture, and a brief description to help aid you. I hope you have fun, and learn somethig new about our beautiful parks.</p>
+        <div class="question-slider-container">
+            <input type='range' min='1' max='63' value='63' class='question-slider w-100' />
+            <p class="green text-center">Number of Questions: <span class='amber question-display'>63</span></p>
+        </div>
+        <input type='button' class='btn btn-green-outline btn-start' value='Start' />
+    </div>
+    `;
+}
+
+function renderResultsPage() {
+    let results = `
+        <div class='results-container amber'>
+            <h1 class='text-center'>Thank You For Playing</h1>
+            <h2>Results:</h2>
+            <div class='results-table-container'>
+                <table class='results-table'>
+                    <tbody>
+                        <tr>
+                            <td>Total</td>
+                            <td>${(document.querySelector('.score').innerHTML)}/${document.querySelector('.question-total').innerHTML}</td>
+                            <td>${Math.floor(parseInt(document.querySelector('.score').innerHTML)/parseInt(document.querySelector('.question-total').innerHTML)*100)}%</td>
+                        </tr>
+    `;
+
+    for (let i in stateNameList) {
+        if (states[i].numberOfParks > 0) {
+            results += `
+                <tr>
+                    <td>${getStateFullName(i)}</td>
+                    <td>${states[i].correctParks}/${states[i].numberOfParks}</td>
+                    <td>${Math.floor(states[i].correctParks/states[i].numberOfParks*100)}%</td>
+                </tr>
+            `;
+        }
+    }
+
+    results += `
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class='flex'>
+            <input class='btn btn-amber-outline btn-restart' type="button" value='Restart' />
+        </div>
+    `;
+
+    return results;
+}
+
+function renderGamePage() {
+    let game = `
+    <div class="question-mask"  style='z-index: 10;'>
+        <div class="question-container crt">
+            <h2 class="park-name bold italic text-center"></h2>
+            <img class='park-img' src="" alt="">
+            <p class="park-description"></p>
+        </div>
+    </div>
+
+    <div class="answer-mask" style='z-index: 5;'>
+        <div class='answer-container green crt'>
+            <i class="far fa-9x"></i>
+            <h1></h1>
+            <p class='state-answer'></p>
+            <input class='btn btn-next'type='button' value='Next' />
+        </div>
+    </div>
+
+    <div class="buttons-container">
+        <div class="input-buttons">
+            <input class='text-input' list="states" name="state" placeholder="State"/>
+            <datalist id="states">
+                <option value="Alabama">Alabama</option>
+                <option value="Alaska">Alaska</option>
+                <option value='American Samoa'>American Samoa</option>
+                <option value="Arizona">Arizona</option>
+                <option value="Arkansas">Arkansas</option>
+                <option value="California">California</option>
+                <option value="Colorado">Colorado</option>
+                <option value="Connecticut">Connecticut</option>
+                <option value="Delaware">Delaware</option>
+                <option value="District of Columbia">District of Columbia</option>
+                <option value="Florida">Florida</option>
+                <option value="Georgia">Georgia</option>
+                <option value="Hawaii">Hawaii</option>
+                <option value="Idaho">Idaho</option>
+                <option value="Illinois">Illinois</option>
+                <option value="Indiana">Indiana</option>
+                <option value="Iowa">Iowa</option>
+                <option value="Kansas">Kansas</option>
+                <option value="Kentucky">Kentucky</option>
+                <option value="Louisiana">Louisiana</option>
+                <option value="Maine">Maine</option>
+                <option value="Maryland">Maryland</option>
+                <option value="Massachusetts">Massachusetts</option>
+                <option value="Michigan">Michigan</option>
+                <option value="Minnesota">Minnesota</option>
+                <option value="Mississippi">Mississippi</option>
+                <option value="Missouri">Missouri</option>
+                <option value="Montana">Montana</option>
+                <option value="Nebraska">Nebraska</option>
+                <option value="Nevada">Nevada</option>
+                <option value="New Hampshire">New Hampshire</option>
+                <option value="New Jersey">New Jersey</option>
+                <option value="New Mexico">New Mexico</option>
+                <option value="New York">New York</option>
+                <option value="North Carolina">North Carolina</option>
+                <option value="North Dakota">North Dakota</option>
+                <option value="Ohio">Ohio</option>
+                <option value="Oklahoma">Oklahoma</option>
+                <option value="Oregon">Oregon</option>
+                <option value="Pennsylvania">Pennsylvania</option>
+                <option value="Rhode Island">Rhode Island</option>
+                <option value="South Carolina">South Carolina</option>
+                <option value="South Dakota">South Dakota</option>
+                <option value="Tennessee">Tennessee</option>
+                <option value="Texas">Texas</option>
+                <option value="Utah">Utah</option>
+                <option value="Vermont">Vermont</option>
+                <option value='Virgin Islands'>Virgin Islands</option>
+                <option value="Virginia">Virginia</option>
+                <option value="Washington">Washington</option>
+                <option value="West Virginia">West Virginia</option>
+                <option value="Wisconsin">Wisconsin</option>
+                <option value="Wyoming">Wyoming</option>
+            </datalist>
+
+            <input class='btn btn-amber-outline btn-submit' type="button" value='Submit' />
+        </div>
+
+        <input class='btn btn-green-outline btn-hint' type="button" value='Hint' />
+    </div>
+    `;
+
+    return game;
+}
--- script.js
+++ script.js
@@ -1,11 +1,9 @@
+
+
 const wipeTransitionTime = 1000; // in milliseconds
 
 function getNPSData(callback) {
-    fetch('https://developer.nps.gov/api/v1/parks?limit=500', {
-        method: 'GET',
-        headers: {
-            'x-api-key': 'b0CyU5w8dbQg3kx1IPbQA2IaDt17EeLvIkcHbdCj'
-        }
+    fetch('nps.json', {
     })
     .then( res => {return res.json()})
     .then( jsonRes => callback(jsonRes));
@@ -41,7 +39,7 @@ function renderQuestion(park) {
         .replace(/&/, '')
         .replace(/(river.*)(river)/ig, '$1');
         document.querySelector('.park-img').src = img.src;
-        document.querySelector('.park-description').innerHTML = park.description;
+        document.querySelector('.park-description').innerHTML = park.description.replace(/Alabama|Alaska|American Samoa|Arizona|Arkansa|California|Colorado|Connecticut|Delaware|District of Columbia|Federated States of Micronesia|Florida|Georgia|Guam|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Marshall Islands|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New Hampshire|New Jersey|New Mexico|New York|North Carolina|North Dakota|Northern Mariana Islands|Ohio|Oklahoma|Oregon|Palau|Pennsylvania|Puerto Rico|Rhode Island|South Carolina|South Dakota|Tennessee|Texas|Utah|Vermont|Virgin Island|Virginia|Washington|West Virginia|Wisconsin|Wyoming/ig, '_____');
     })
 
     img.src = park.images[0].url;
@@ -74,6 +72,8 @@ function displayQuestion() {
 
 function renderAnswer(correctAnswer, park) {
     const userAnswer = getStateTwoDigitCode(document.querySelector('.text-input').value);
+    let correctStates = correctAnswer.split(',');
+
     if (correctAnswer.includes(userAnswer)) {
         console.log('yay!')
         document.querySelector('.answer-container').className = document.querySelector('.answer-container').className.replace('red', 'green');
@@ -82,15 +82,23 @@ function renderAnswer(correctAnswer, park) {
         document.querySelector('.answer-container input').classList.add('btn-green-outline');
         document.querySelector('.answer-container input').classList.remove('btn-red-outline');
         document.querySelector('.score').setAttribute('data-score', parseInt(document.querySelector('.score').innerHTML) + 1)
+        for (let i of correctStates) {
+            console.log(i)
+            states[i].numberOfParks++;
+            states[i].correctParks++;
+        }
     } else {
         document.querySelector('.answer-container').className = document.querySelector('.answer-container').className.replace('green', 'red');
         document.querySelector('.answer-container i').className = 'fas fa-times fa-9x';
         document.querySelector('.answer-container h1').innerHTML = '';
         document.querySelector('.answer-container input').classList.add('btn-red-outline');
         document.querySelector('.answer-container input').classList.remove('btn-green-outline');
+        for (let i of correctStates) {
+            states[i].numberOfParks++;
+        }
     }
 
-    let correctStates = correctAnswer.split(',');
+ 
     let stateNamesString = '';
     for (let i in correctStates) {
         if (i > 0) {
@@ -98,7 +106,7 @@ function renderAnswer(correctAnswer, park) {
         }
         stateNamesString += getStateFullName(correctStates[i]);
     }
-    //document.querySelector('.state-answer').innerHTML = stateNamesString + ' - ' + park.fullName.replace(/national|preserve|park/ig, '');
+
     document.querySelector('.state-answer').innerHTML = stateNamesString + ' - ' + park.fullName.replace(/\b(national+.*of|national|preserve|park|parks|state|and)\b/ig, '')
     .replace(/&/, '')
     .replace(/(river.*)(river)/ig, '$1');
@@ -123,45 +131,147 @@ function displayAnsewr() {
     }, wipeTransitionTime)
 }
 
-getNPSData( (jsonRes) => {
-    const data = jsonRes.data;
+
+function menuPage() {
+    document.querySelector('.card').innerHTML = renderMenuPage();
+
+    document.querySelector('.status').classList.add('hidden');
+
+    document.querySelector('.question-slider').addEventListener('input', () => {
+        document.querySelector('.question-display').innerHTML = document.querySelector('.question-slider').value;
+    })
+
+    document.querySelector('.btn-start').addEventListener('click', () => {
+        getNPSData( (res) => {
+            gamePage(res.data, document.querySelector('.question-slider').value);
+        })
+    });
+}
+
+function gamePage(data, numberOfParks) {
+
+    document.querySelector('.card').innerHTML = renderGamePage();
+
+    document.querySelector('.status').classList.remove('hidden');
+
+    // Reset states object in case user restarts
+    for (let i in states) {
+        states[i].numberOfParks = 0;
+        states[i].correctParks = 0;
+    }
+
     let parks = [];
 
     for (let i in data) {
-        if (data[i].designation.includes("National Park")) parks.push(data[i]);  
-        // Manually adding American Samoa, Redwood, and New River because the NPS API is inconsistant with its categorization
-        if (data[i].fullName == 'National Park of American Samoa' || data[i].fullName == 'Redwood National and State Parks' || data[i].fullName == 'New River Gorge National River') parks.push(data[i]);
+        if (data[i].designation.includes("National Park")) parks.push(data[i]); 
     }
 
     let parkIterator = 0;
-    const numberOfParks = parks.length;
     document.querySelector('.question-total').innerHTML = numberOfParks;
 
     generateRandomParkOrder(parks);
-     
+    
     let correctAnswer = renderQuestion(parks[parkIterator]);
-
 
     document.addEventListener('keyup', (e) => {
         if (e.key == 'Enter') {
             if (document.querySelector('.question-mask').style.zIndex == 10 && !document.querySelector('.btn-submit').classList.contains('disabled')) {
-                renderAnswer(correctAnswer, parks[parkIterator++]);
-                displayAnsewr();
-                setTimeout(() => {correctAnswer = renderQuestion(parks[parkIterator])}, wipeTransitionTime );
+                // When question is displayed
+                submitBtnHandler();
             } else  if (document.querySelector('.answer-mask').style.zIndex == 10 && !document.querySelector('.btn-next').classList.contains('disabled')) {
-                displayQuestion();
+                // When answer is displayed
+                nextBtnHandler();
             }
         }
     })
 
-    document.querySelector('.btn-submit').addEventListener('click', (e) => {
-        renderAnswer(correctAnswer, parks[parkIterator++]);
-        displayAnsewr();
-        setTimeout(() => {correctAnswer = renderQuestion(parks[parkIterator])}, wipeTransitionTime );
+    document.querySelector('.btn-submit').addEventListener('click', () => {
+        submitBtnHandler();
     });
 
     document.querySelector('.btn-next').addEventListener('click', () => {
-        displayQuestion();
+        nextBtnHandler()
     })
 
-});
+    function submitBtnHandler() {
+        renderAnswer(correctAnswer, parks[parkIterator++]);
+        displayAnsewr();
+        setTimeout(() => {correctAnswer = renderQuestion(parks[parkIterator])}, wipeTransitionTime );
+    }
+
+    function nextBtnHandler() {
+        if (parkIterator < numberOfParks) {
+            displayQuestion();
+        } else {
+            resultPage();
+        }
+    }
+
+}
+
+function resultPage() {
+    document.querySelector('.card').innerHTML = renderResultsPage();
+
+    document.querySelector('.btn-restart').addEventListener('click', () => {
+        menuPage();
+    })
+}
+
+
+menuPage();
+
+states = {
+    AZ: {numberOfParks: 0, correctParks:  0} ,
+    AL: {numberOfParks: 0, correctParks:  0},
+    AK: {numberOfParks: 0, correctParks:  0},
+    AS: {numberOfParks: 0, correctParks:  0},
+    AR: {numberOfParks: 0, correctParks:  0},
+    CA: {numberOfParks: 0, correctParks:  0},
+    CO: {numberOfParks: 0, correctParks:  0},
+    CT: {numberOfParks: 0, correctParks:  0},
+    DC: {numberOfParks: 0, correctParks:  0},
+    DE: {numberOfParks: 0, correctParks:  0},
+    FL: {numberOfParks: 0, correctParks:  0},
+    GA: {numberOfParks: 0, correctParks:  0},
+    HI: {numberOfParks: 0, correctParks:  0},
+    ID: {numberOfParks: 0, correctParks:  0},
+    IL: {numberOfParks: 0, correctParks:  0},
+    IN: {numberOfParks: 0, correctParks:  0},
+    IA: {numberOfParks: 0, correctParks:  0},
+    KS: {numberOfParks: 0, correctParks:  0},
+    KY: {numberOfParks: 0, correctParks:  0},
+    LA: {numberOfParks: 0, correctParks:  0},
+    ME: {numberOfParks: 0, correctParks:  0},
+    MD: {numberOfParks: 0, correctParks:  0},
+    MA: {numberOfParks: 0, correctParks:  0},
+    MI: {numberOfParks: 0, correctParks:  0},
+    MN: {numberOfParks: 0, correctParks:  0},
+    MS: {numberOfParks: 0, correctParks:  0},
+    MO: {numberOfParks: 0, correctParks:  0},
+    MT: {numberOfParks: 0, correctParks:  0},
+    NE: {numberOfParks: 0, correctParks:  0},
+    NV: {numberOfParks: 0, correctParks:  0},
+    NH: {numberOfParks: 0, correctParks:  0},
+    NJ: {numberOfParks: 0, correctParks:  0},
+    NM: {numberOfParks: 0, correctParks:  0},
+    NY: {numberOfParks: 0, correctParks:  0},
+    NC: {numberOfParks: 0, correctParks:  0},
+    ND: {numberOfParks: 0, correctParks:  0},
+    OH: {numberOfParks: 0, correctParks:  0},
+    OK: {numberOfParks: 0, correctParks:  0},
+    OR: {numberOfParks: 0, correctParks:  0},
+    PA: {numberOfParks: 0, correctParks:  0},
+    RI: {numberOfParks: 0, correctParks:  0},
+    SC: {numberOfParks: 0, correctParks:  0},
+    SD: {numberOfParks: 0, correctParks:  0},
+    TN: {numberOfParks: 0, correctParks:  0},
+    TX: {numberOfParks: 0, correctParks:  0},
+    UT: {numberOfParks: 0, correctParks:  0},
+    VT: {numberOfParks: 0, correctParks:  0},
+    VI: {numberOfParks: 0, correctParks:  0},
+    VA: {numberOfParks: 0, correctParks:  0},
+    WA: {numberOfParks: 0, correctParks:  0},
+    WV: {numberOfParks: 0, correctParks:  0},
+    WI: {numberOfParks: 0, correctParks:  0},
+    WY: {numberOfParks: 0, correctParks:  0} 
+}
